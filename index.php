4fbcb896c22b85f13dae3bddade8c4962b2927a8
aaditya-biswas <147297053+aaditya-biswas@users.noreply.github.com>
2025-04-02 15:47:10 +0530

    PHP script
    Login Php script

--- index.php
+++ index.php
@@ -1,9 +1,44 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve the input value using the name attribute
-    $username = htmlspecialchars($_POST['username']);
-    
-    // Now you can use the $username variable as needed
-    //echo "The submitted username is: " . $username;
+session_start();
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "se_project";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $name = $_POST["name"] ?? '';
+    $password = $_POST["Password"] ?? '';
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE Username= ? AND Password = ?");
+    $stmt->bind_param("ss", $name, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $_SESSION["name"] = $_POST["name"];
+    $_SESSION["Password"] = $_POST["Password"];
+    if ($result->num_rows > 0) {
+      ob_start(); // Start output buffering
+
+      header("Location: https://localhost/registercourse.php");
+      
+      exit(); // Stop further execution
+    }
+   else {
+      echo"<script type='text/JavaScript'>  
+      alert('Invalid Credentials'); 
+      </script>" ;
+      header("Location: https://localhost/index.html");
+      
+    }
+    $conn->close();
+}
+else {
+  
 }
 ?>
